e5b43593c45c5c512c83d83efab6720ef9b4ee27
msakuta <masahiro.sakuta@gmail.com>
2024-10-15 02:48:29 +0900

    Separate disassembly window

--- cli/src/debugger.rs
+++ cli/src/debugger.rs
@@ -31,6 +31,7 @@ pub(crate) fn run_debugger(bytecode: &Bytecode) -> Result<(), Box<dyn std::error
 struct App<'a> {
     bytecode: &'a Bytecode,
     mode: AppMode<'a>,
+    disasm: Option<DisasmWidget>,
     exit: bool,
 }
 
@@ -39,6 +40,7 @@ impl<'a> App<'a> {
         Self {
             bytecode,
             mode: Default::default(),
+            disasm: None,
             exit: false,
         }
     }
@@ -59,12 +61,11 @@ impl<'a> App<'a> {
         if let event::Event::Key(key) = event::read()? {
             match (key.kind, key.code) {
                 (KeyEventKind::Press, KeyCode::Char('d')) => {
-                    let mut temp = vec![];
-                    self.bytecode.disasm(&mut temp)?;
-                    self.mode = AppMode::Disasm {
-                        text: String::from_utf8(temp)?,
-                        scroll: 0,
-                    };
+                    if self.disasm.is_some() {
+                        self.disasm = None;
+                    } else {
+                        self.disasm = DisasmWidget::new(self.bytecode).ok();
+                    }
                 }
                 (KeyEventKind::Press, KeyCode::Char('r')) => {
                     interpret(self.bytecode)?;
@@ -94,13 +95,13 @@ impl<'a> App<'a> {
                     }
                 }
                 (KeyEventKind::Press, KeyCode::Up) => {
-                    if let AppMode::Disasm { ref mut scroll, .. } = self.mode {
-                        *scroll = scroll.saturating_sub(1)
+                    if let Some(ref mut da) = self.disasm {
+                        da.scroll = da.scroll.saturating_sub(1)
                     }
                 }
                 (KeyEventKind::Press, KeyCode::Down) => {
-                    if let AppMode::Disasm { ref mut scroll, .. } = self.mode {
-                        *scroll += 1
+                    if let Some(ref mut da) = self.disasm {
+                        da.scroll += 1
                     }
                 }
                 _ => {}
@@ -132,18 +133,8 @@ impl<'a> Widget for &App<'a> {
             )
             .border_set(border::THICK);
 
-        let disasm_msg = "Showing disassembly (q to quit)";
-
         let inner_text = match &self.mode {
             AppMode::None => Text::from(vec![Line::from("Press Q to exit")]),
-            AppMode::Disasm { text, scroll } => {
-                let text_lines: Vec<_> = text.split('\n').collect();
-                let mut lines = vec![format!("{disasm_msg}: {scroll}/{}", text_lines.len()).into()];
-                if *scroll < text_lines.len() {
-                    lines.extend(text_lines[*scroll..].iter().map(|v| Line::from(*v)));
-                }
-                Text::from(lines)
-            }
             AppMode::StepRun { vm, error } => {
                 if let Some(call_info) = vm.top_call_info() {
                     let mut lines =
@@ -176,6 +167,12 @@ impl<'a> Widget for &App<'a> {
         };
 
         Paragraph::new(inner_text).block(block).render(area, buf);
+
+        let mut disasm_area = area;
+        disasm_area.y = disasm_area.height / 2;
+        disasm_area.height /= 2;
+
+        self.disasm.as_ref().map(|d| d.render(disasm_area, buf));
     }
 }
 
@@ -183,12 +180,46 @@ impl<'a> Widget for &App<'a> {
 enum AppMode<'a> {
     #[default]
     None,
-    Disasm {
-        text: String,
-        scroll: usize,
-    },
     StepRun {
         vm: Vm<'a>,
         error: RefCell<Option<String>>,
     },
+}
+
+struct DisasmWidget {
+    text: String,
+    scroll: usize,
+}
+
+impl DisasmWidget {
+    fn new(bytecode: &Bytecode) -> Result<Self, Box<dyn std::error::Error>> {
+        let mut temp = vec![];
+        bytecode.disasm(&mut temp)?;
+        Ok(Self {
+            text: String::from_utf8(temp)?,
+            scroll: 0,
+        })
+    }
+}
+
+impl Widget for &DisasmWidget {
+    fn render(self, area: Rect, buf: &mut Buffer)
+    where
+        Self: Sized,
+    {
+        let title = Title::from(" Disassembly ".bold());
+        let block = Block::bordered()
+            .title(title.alignment(Alignment::Center))
+            .border_set(border::THICK);
+
+        let text_lines: Vec<_> = self.text.split('\n').collect();
+        let mut lines = vec![format!("{}/{}", self.scroll, text_lines.len()).into()];
+        if self.scroll < text_lines.len() {
+            lines.extend(text_lines[self.scroll..].iter().map(|v| Line::from(*v)));
+        }
+
+        Paragraph::new(Text::from(lines))
+            .block(block)
+            .render(area, buf);
+    }
 }
