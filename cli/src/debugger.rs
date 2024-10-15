49f6ba7ab7f51b39fbe4c5de99b4c6a601cbd3b0
msakuta <masahiro.sakuta@gmail.com>
2024-10-16 01:16:41 +0900

    Add a dedicated help widget to show all key bindings

--- cli/src/debugger.rs
+++ cli/src/debugger.rs
@@ -1,6 +1,7 @@
 //! Implementation of the interactive debugger.
 
 mod disasm;
+mod help;
 mod stack;
 mod stack_trace;
 
@@ -22,7 +23,9 @@ use ratatui::{
 
 use ::mascal::{interpret, Bytecode, Vm};
 
-use self::{disasm::DisasmWidget, stack::StackWidget, stack_trace::StackTraceWidget};
+use self::{
+    disasm::DisasmWidget, help::HelpWidget, stack::StackWidget, stack_trace::StackTraceWidget,
+};
 
 pub(crate) fn run_debugger(bytecode: &Bytecode) -> Result<(), Box<dyn std::error::Error>> {
     let mut terminal = ratatui::init();
@@ -39,6 +42,7 @@ struct App<'a> {
     disasm: Option<DisasmWidget>,
     stack_trace: Option<StackTraceWidget>,
     stack: Option<StackWidget>,
+    help: Option<HelpWidget>,
     exit: bool,
 }
 
@@ -50,6 +54,7 @@ impl<'a> App<'a> {
             disasm: DisasmWidget::new(bytecode).ok(),
             stack_trace: StackTraceWidget::new().ok(),
             stack: StackWidget::new().ok(),
+            help: None,
             exit: false,
         }
     }
@@ -88,6 +93,13 @@ impl<'a> App<'a> {
                         self.stack = None;
                     } else {
                         self.stack = StackWidget::new().ok();
+                    }
+                }
+                (KeyEventKind::Press, KeyCode::Char('h')) => {
+                    if self.help.is_some() {
+                        self.help = None;
+                    } else {
+                        self.help = HelpWidget::new().ok();
                     }
                 }
                 (KeyEventKind::Press, KeyCode::Char('r')) => {
@@ -155,91 +167,98 @@ impl<'a> App<'a> {
 impl<'a> Widget for &App<'a> {
     fn render(self, area: Rect, buf: &mut Buffer) {
         let title = Title::from(" Interactive debugger ".bold());
-        let instructions = Title::from(Line::from(vec![
-            "  disassemby: ".into(),
-            "d".blue().bold(),
-            "  stack trace: ".into(),
-            "t".blue().bold(),
-            "  Show local stack values: ".into(),
-            "l".blue().bold(),
-            "  run current code: ".into(),
-            "r".blue().bold(),
-            "  Step execution mode: ".into(),
-            "s".blue().bold(),
-            "  quit: ".into(),
-            "q ".blue().bold(),
-        ]));
-        let block = Block::bordered()
-            .title(title.alignment(Alignment::Center))
-            .title(
-                instructions
-                    .alignment(Alignment::Center)
-                    .position(Position::Bottom),
-            )
-            .border_set(border::THICK);
+        // Help shows on top of all widgets
+        if let Some(ref help) = self.help {
+            let mut help_area = area;
+            help_area.x += help_area.width / 4;
+            help_area.width /= 2;
+            help_area.y += help_area.height / 4;
+            help_area.height /= 2;
+            help.render(help_area, buf);
+        } else {
+            let instructions = Title::from(Line::from(vec![
+                "  help: ".into(),
+                "h".blue().bold(),
+                "  quit: ".into(),
+                "q ".blue().bold(),
+            ]));
+            let block = Block::bordered()
+                .title(title.alignment(Alignment::Center))
+                .title(
+                    instructions
+                        .alignment(Alignment::Center)
+                        .position(Position::Bottom),
+                )
+                .border_set(border::THICK);
 
-        let inner_text = match &self.mode {
-            AppMode::None => Text::from(vec![Line::from("Press Q to exit")]),
-            AppMode::StepRun { vm, error } => {
-                if let Some(call_info) = vm.top_call_info() {
-                    let mut lines =
-                        vec![format!("Execution state at {}", call_info.instuction_ptr()).into()];
+            let inner_text = match &self.mode {
+                AppMode::None => Text::from(vec![
+                    Line::from("Press Q to exit"),
+                    Line::from("Press H for help"),
+                ]),
+                AppMode::StepRun { vm, error } => {
+                    if let Some(call_info) = vm.top_call_info() {
+                        let mut lines =
+                            vec![
+                                format!("Execution state at {}", call_info.instuction_ptr()).into()
+                            ];
 
-                    let mut buf = vec![];
-                    if let Err(e) = vm.dump_stack(&mut buf) {
-                        *error.borrow_mut() = Some(e.to_string());
-                    }
-                    if let Ok(s) = String::from_utf8(buf) {
-                        lines.push(s.into());
-                    }
+                        let mut buf = vec![];
+                        if let Err(e) = vm.dump_stack(&mut buf) {
+                            *error.borrow_mut() = Some(e.to_string());
+                        }
+                        if let Ok(s) = String::from_utf8(buf) {
+                            lines.push(s.into());
+                        }
 
-                    let mut buf = vec![];
-                    if let Err(e) = vm.format_current_inst(&mut buf) {
-                        *error.borrow_mut() = Some(e.to_string());
-                    }
-                    if let Ok(s) = String::from_utf8(buf) {
-                        lines.push(s.into());
-                    }
+                        let mut buf = vec![];
+                        if let Err(e) = vm.format_current_inst(&mut buf) {
+                            *error.borrow_mut() = Some(e.to_string());
+                        }
+                        if let Ok(s) = String::from_utf8(buf) {
+                            lines.push(s.into());
+                        }
 
-                    if let Some(error) = error.borrow().as_ref() {
-                        lines.push(format!("Error: {error}").into());
+                        if let Some(error) = error.borrow().as_ref() {
+                            lines.push(format!("Error: {error}").into());
+                        }
+                        Text::from(lines)
+                    } else {
+                        Text::from("Step exection has not started yet.")
                     }
-                    Text::from(lines)
-                } else {
-                    Text::from("Step exection has not started yet.")
                 }
-            }
-        };
+            };
 
-        Paragraph::new(inner_text).block(block).render(area, buf);
+            Paragraph::new(inner_text).block(block).render(area, buf);
 
-        let mut tr_area = area;
-        if 0 < tr_area.height {
-            tr_area.x = area.width / 2;
-            tr_area.width /= 2;
-            tr_area.y += 1;
-            tr_area.height = (tr_area.height - 1) / 2;
-            self.stack.as_ref().map(|d| d.render(tr_area, buf));
-        }
-
-        let mut widget_area = area;
-        if 0 < widget_area.height {
-            widget_area.y = widget_area.height / 2;
-            widget_area.height = (widget_area.height - 1) / 2;
-
-            let widget_count = self.disasm.is_some() as u16 + self.stack_trace.is_some() as u16;
-
-            if widget_count != 0 {
-                widget_area.width /= widget_count;
+            let mut tr_area = area;
+            if 0 < tr_area.height {
+                tr_area.x = area.width / 2;
+                tr_area.width /= 2;
+                tr_area.y += 1;
+                tr_area.height = (tr_area.height - 1) / 2;
+                self.stack.as_ref().map(|d| d.render(tr_area, buf));
             }
 
-            if let Some(d) = self.disasm.as_ref() {
-                d.render(widget_area, buf);
-                widget_area.x += widget_area.width;
+            let mut widget_area = area;
+            if 0 < widget_area.height {
+                widget_area.y = widget_area.height / 2;
+                widget_area.height = (widget_area.height - 1) / 2;
+
+                let widget_count = self.disasm.is_some() as u16 + self.stack_trace.is_some() as u16;
+
+                if widget_count != 0 {
+                    widget_area.width /= widget_count;
+                }
+
+                if let Some(d) = self.disasm.as_ref() {
+                    d.render(widget_area, buf);
+                    widget_area.x += widget_area.width;
+                }
+                self.stack_trace
+                    .as_ref()
+                    .map(|d| d.render(widget_area, buf));
             }
-            self.stack_trace
-                .as_ref()
-                .map(|d| d.render(widget_area, buf));
         }
     }
 }
---
+++ cli/src/debugger/help.rs
@@ -0,0 +1,48 @@
+use ratatui::{
+    buffer::Buffer,
+    layout::{Alignment, Rect},
+    style::{Color, Style, Stylize},
+    symbols::border,
+    text::{Line, Text},
+    widgets::{block::Title, Block, Paragraph, Widget},
+};
+
+pub(super) struct HelpWidget {}
+
+impl HelpWidget {
+    pub(super) fn new() -> Result<Self, Box<dyn std::error::Error>> {
+        Ok(Self {})
+    }
+}
+
+impl Widget for &HelpWidget {
+    fn render(self, area: Rect, buf: &mut Buffer)
+    where
+        Self: Sized,
+    {
+        let text_lines = Text::from(vec![
+            Line::from(vec![
+                "  Toggle help (this window): ".into(),
+                "h".blue().bold(),
+            ]),
+            Line::from(vec!["  Toggle disassembly: ".into(), "d".blue().bold()]),
+            Line::from(vec!["  Toggle stack trace: ".into(), "t".blue().bold()]),
+            Line::from(vec![
+                "  Toggle local stack values: ".into(),
+                "l".blue().bold(),
+            ]),
+            Line::from(vec!["  run current code: ".into(), "r".blue().bold()]),
+            Line::from(vec!["  Step execution mode: ".into(), "s".blue().bold()]),
+            Line::from(vec!["  quit: ".into(), "q ".blue().bold()]),
+        ]);
+        let title = Title::from(" Help ".bold());
+        let block = Block::bordered()
+            .title(title.alignment(Alignment::Center))
+            // .bg(Color::Blue)
+            .style(Style::default().bg(Color::DarkGray))
+            .border_style(Style::new().white())
+            .border_set(border::THICK);
+
+        Paragraph::new(text_lines).block(block).render(area, buf);
+    }
+}
