1f84b4e06a5c5975dd9298d6e828f5e2e31a503a
msakuta <masahiro.sakuta@gmail.com>
2026-07-14 23:58:11 +0900

    Support return statement in bytecode
    Actually, it was simply an early return.

--- parser/src/compiler.rs
+++ parser/src/compiler.rs
@@ -600,10 +600,12 @@ fn emit_stmts<'src>(
                 compiler.break_ips.push(break_ip);
             }
             Statement::Return(ex) => {
-                if let Some(ex) = ex {
-                    emit_expr(ex, compiler)?.into_stack(compiler);
-                }
-                compiler.push_inst(OpCode::Ret, 0, 0);
+                let res = if let Some(ex) = ex {
+                    emit_expr(ex, compiler)?.into_stack(compiler)
+                } else {
+                    0
+                };
+                compiler.push_inst(OpCode::Ret, 0, res as u16);
             }
             // For now, structs are concepts only at compile time.
             Statement::Struct(st) => {
